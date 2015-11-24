13aa1367376b81c50bf0d842cb2e9ecfec09e33d
Feiyang <85230316@qq.com>
2015-11-24 16:46:33 +0800

    Removed repeated form class

--- EventManager/app/basic/forms.py
+++ EventManager/app/basic/forms.py
@@ -43,16 +43,5 @@ class JoinForm(Form):
 	last_name = StringField('Last Name', validators=[InputRequired(), Length(max=10)])
 
 
-class CreateEventForm(Form):
-	topic = StringField('Topic', validators=[InputRequired()])
-	description = StringField('Description', validators=[InputRequired()])
-	min_attendance = IntegerField('Minimal attendance', default=10)
-	max_attendance = IntegerField('Maximal attendance', default=200)
-	location = StringField('Location', validators=[InputRequired()], default='TBD')
-	host = StringField('Host', validators=[InputRequired()], default='TBD')
-	duration = IntegerField("Event Duration", default=30, validators=[InputRequired()])
-	start_date = DateField("Event Starts at", validators=[InputRequired()], id="start_date")
-
-
 class PwdResetForm(Form):
 	email = StringField('Email', validators=[InputRequired(), email_check, Email(message="Please input a valid Email address")])
