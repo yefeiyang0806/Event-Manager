4d6589471482430d0d235b463ba3e5338226ca4f
panry <panrui23@foxmail.com>
2015-12-01 10:08:13 +0800

    little cahnge

--- EventManager/app/role/forms.py
+++ EventManager/app/role/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo, Validation
 
 
 class CreateRoleForm(Form):
-	rolename = StringField('Rolename', validators=[InputRequired()])
+	rolename = StringField('Rolename', validators=[InputRequired(), Length(min=4, max=20)], default="Length no more than 20")
 	description = StringField('Description', validators=[InputRequired()])
